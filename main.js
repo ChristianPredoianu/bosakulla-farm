72f3a186864fc2a4368954f64a05ce0341c849b6
Christian Predoianu <christianpredoianu@yahoo.com>
2022-09-15 18:18:43 +0200

    work

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+const navItemLinks = document.querySelectorAll('.nav-item');
 
+function toggleNavLinks() {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
+}
+
+function closeNavLinks() {
+  hamburger.classList.remove('active');
+  navLinks.classList.remove('active');
+}
+
+navItemLinks.forEach((navItemLink) =>
+  navItemLink.addEventListener('click', function () {
+    closeNavLinks();
+    navItemLinks.forEach((navItemLink) =>
+      navItemLink.classList.remove('active-link')
+    );
+    this.classList.add('active-link');
+  })
+);
+
+window.addEventListener('resize', closeNavLinks);
+hamburger.addEventListener('click', toggleNavLinks);
