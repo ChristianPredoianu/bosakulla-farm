f11c92fae942b364198ad7c03136ac22dbda8e3a
Christian Predoianu <christianpredoianu@yahoo.com>
2022-09-16 17:56:50 +0200

    work

--- main.js
+++ main.js
@@ -24,3 +24,16 @@ navItemLinks.forEach((navItemLink) =>
 
 window.addEventListener('resize', closeNavLinks);
 hamburger.addEventListener('click', toggleNavLinks);
+
+//Swiper
+
+const swiper = new Swiper('.mySwiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
