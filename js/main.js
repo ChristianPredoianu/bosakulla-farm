3a86b44f907cea8466af1fba13e687305a983879
Christian Predoianu <christianpredoianu@yahoo.com>
2022-09-28 15:56:07 +0200

    final

--- js/main.js
+++ js/main.js
@@ -7,12 +7,7 @@ import { setActiveBulletClick, setActivebulletScroll } from './pagination';
 const ctaBtns = document.querySelectorAll('.cta-btn'),
   modalBg = document.getElementById('modal-bg'),
   paginationBullets = document.querySelectorAll('.pagination__bullet'),
-  sections = document.querySelectorAll('section'),
-  modalForm = document.getElementById('modal-form'),
-  footerForm = document.getElementById('footer-form'),
-  modalMsgInput = document.getElementById('modal-msg-input'),
-  footerMsgInput = document.getElementById('footer-msg-input'),
-  footerMap = document.getElementById('footer-map');
+  sections = document.querySelectorAll('section');
 
 nav();
 swiper();
