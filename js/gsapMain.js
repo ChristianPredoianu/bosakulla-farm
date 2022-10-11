653c4613bab119041583d6badee0a86ba85628d7
Christian Predoianu <christianpredoianu@yahoo.com>
2022-10-11 08:51:02 +0200

    added matchMedia snap scroll only on md devices

--- js/gsapMain.js
+++ js/gsapMain.js
@@ -1,12 +1,16 @@
 export default () => {
   gsap.registerPlugin(ScrollTrigger);
 
-  gsap.utils.toArray('.snap-section').forEach((layer) => {
-    ScrollTrigger.create({
-      trigger: layer,
-      start: 'top top',
-      snap: 1,
-      duration: 0.1,
+  let mm = gsap.matchMedia();
+
+  mm.add('(min-width: 1024px)', () => {
+    gsap.utils.toArray('.snap-section').forEach((layer) => {
+      ScrollTrigger.create({
+        trigger: layer,
+        start: 'top top',
+        snap: 1,
+        duration: 0.1,
+      });
     });
   });
 };
